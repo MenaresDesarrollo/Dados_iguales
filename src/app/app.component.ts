c6af1d80c3c366d5c790059142c8c36be0a7913f
Carlos Menares <menaresdesarrollo@gmail.com>
2023-03-03 17:52:04 -0300

    juego de dados unico commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dados';
+  dadoIzquierda = '../assets/img/dice1.png';
+  dadoDerecha = '../assets/img/dice4.png';
+  numero1 = 1;
+  numero2 = 2;
+
+  tirarDados(): void{
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+    console.log(this.numero1);
+    console.log(this.numero2);
+
+    this.dadoIzquierda = '../assets/img/dice' +this.numero1 +'.png';
+    this.dadoDerecha = '../assets/img/dice' +this.numero2 +'.png';
+  }
 }
